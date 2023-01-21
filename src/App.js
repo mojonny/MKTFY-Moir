ee76f4af71fbcd2bcbb9f8efed36c752ec5548e7
mojonny <moirjonathan@gmail.com>
2023-01-20 17:18:18 -0700

    Reorganized login portal with component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import logo from './assets/MKTFYlogo.png';
 import './App.css';
+import LoginPortal from './components/login-portal.component';
 
 function App() {
 	return (
 		<div className="App">
-			<div className="intro-container">
-				<img src={logo} className="logo" alt="logo" />
-				<button className="login">Login</button>
-				<button className="createPassword">Create account</button>
-				<div className="bottom-text">
-					Find out more about us!
-					<a href="www.google.ca" className="site-link">
-						Visit our website
-					</a>
-				</div>
-			</div>
+			<LoginPortal />
 		</div>
 	);
 }
