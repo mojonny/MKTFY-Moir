3fb1b707635fa1f5fbd7bf772ccfa3f9a378ad93
mojonny <moirjonathan@gmail.com>
2023-01-29 08:09:07 -0700

    Updating for vercel.

---
+++ src/Components/Pages/TOC/TOC.component.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import Chat from '../../../assets/Chat.png';
+import Arrow from '../../../assets/Arrow.png';
+import './TOC.styles.css';
+
+export default function TOC() {
+	const location = useLocation();
+	console.log(location.pathname);
+
+	//let Title = location.pathname;
+
+	let setTitle = (location) => {
+		if ((location.pathname = '/privacy')) {
+			return (
+				<div>
+					<h1>'MKTFY Privacy policy'</h1>
+				</div>
+			);
+		} else {
+			return 'MKTFY Terms and services';
+		}
+	};
+	console.log(setTitle);
+
+	const navigate = useNavigate();
+
+	return (
+		<div className="page-container">
+			<img
+				alt="people chatting"
+				className="chat-image"
+				src={Chat}
+				style={{
+					position: 'fixed',
+					width: '100vw',
+					height: '400px',
+				}}
+			/>
+
+			<div
+				className="info-container"
+				style={{ paddingTop: '500px', paddingLeft: '230px' }}
+			>
+				<button
+					style={{
+						position: 'relative',
+						right: '100px',
+						backgroundColor: '#ffffff',
+						border: 'none',
+					}}
+				>
+					<img src={Arrow} alt="back" onClick={() => navigate(-1)} />
+				</button>
+				<div className="text-container">
+					<h2>{location}</h2>
+					<h3 style={{ textAlign: 'left' }}>
+						These Terms of Service constitute a legally binding agreement made
+						between you, whether personally or on behalf of an entity (“you”)
+						and [business entity name] (“we,” “us” or “our”), concerning your
+						access to and use of the [website name.com] website as well as any
+						other media form, media channel, mobile website or mobile
+						application related, linked, or otherwise connected thereto
+						(collectively, the “Site”).
+						<br />
+						<br />
+						You agree that by accessing the Site, you have read, understood, and
+						agree to be bound by all of these Terms of Service. If you do not
+						agree with all of these Terms of Service, then you are expressly
+						prohibited from using the Site and you must discontinue use
+						immediately.
+						<br />
+						<br />
+						Supplemental Terms of Service or documents that may be posted on the
+						Site from time to time are hereby expressly incorporated herein by
+						reference. We reserve the right, in our sole discretion, to make
+						changes or modifications to these Terms of Service at any time and
+						for any reason.
+						<br />
+						<br />
+						We will alert you about any changes by updating the “Last updated”
+						date of these Terms of Service, and you waive any right to receive
+						specific notice of each such change.
+						<br />
+						<br />
+						It is your responsibility to periodically review these Terms of
+						Service to stay informed of updates. You will be subject to, and
+						will be deemed to have been made aware of and to have accepted, the
+						changes in any revised Terms of Service by your continued use of the
+						Site after the date such revised Terms of Service are posted. The
+						information provided on the Site is not intended for distribution to
+						or use by any person or entity in any jurisdiction or country where
+						such distribution or use would be contrary to law or regulation or
+						which would subject us to any registration requirement within such
+						jurisdiction or country.
+					</h3>
+				</div>
+			</div>
+		</div>
+	);
+}
