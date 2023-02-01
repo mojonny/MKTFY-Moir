9578945af6e84c6a22c34210d1d717f70c2da955
mojonny <moirjonathan@gmail.com>
2023-01-31 20:39:48 -0700

    Only needed slider and ot carousel.

--- src/Components/Slider/index.js
+++ src/Components/Slider/index.js
@@ -4,19 +4,21 @@ import './index.css';
 
 export default function Slider() {
 	const title = 'Deals for you';
-	//will
+
 	return (
 		<div className="slider">
 			<p className="slider-title">{title}</p>
-			<div className="cards">
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
+			<div className="card-container">
+				<div className="cards">
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+				</div>
 			</div>
 		</div>
 	);
