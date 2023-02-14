0b83f22f004c9c9ce84852334d05541fe7f93c31
mojonny <moirjonathan@gmail.com>
2023-02-13 20:36:42 -0700

    Set up profile dropdown and outside click to close

--- src/Components/Header/Dropdown/index.js
+++ src/Components/Header/Dropdown/index.js
@@ -1,94 +1,113 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
-import topArrow from '../../../assets/DropdownTopArrow.png';
 import logout from '../../../assets/LogOut.png';
-
+import dropArrow from '../../../assets/DownArrow.png';
 import './index.css';
 
-export default function Dropdown({ isDropOpen }) {
+export default function Dropdown() {
+	const [isDropOpen3, setIsDropOpen3] = useState(false);
+
+	const dropMenu = useRef(null);
+
+	const closeOpenMenus = (e) => {
+		if (
+			dropMenu.current &&
+			isDropOpen3 &&
+			!dropMenu.current.contains(e.target)
+		) {
+			setIsDropOpen3(false);
+		}
+	};
+	document.addEventListener('mousedown', closeOpenMenus);
+	const handleToggle3 = () => {
+		setIsDropOpen3((current) => !current);
+	};
 	return (
-		<div className="dropBG" onClick={() => isDropOpen(false)}>
-			<div className="drop-container" open={true}>
-				<img
-					src={topArrow}
-					alt="top of box arrow"
-					style={{
-						transform: 'translate(0%, -65%)',
-						marginBottom: '-70px',
-					}}
-				/>
-
-				<div className="title-container">
-					<div
-						style={{
-							color: '#ffffff',
-							backgroundColor: '#560f9f',
-							height: '40px',
-							width: '40px',
-							borderRadius: '200px',
-							padding: '10px 15px 10px 15px',
-						}}
-					>
-						P
-					</div>
-					<h1>Pearl The Cat</h1>
-				</div>
-
-				<h2 style={{ color: 'black', margin: '0px' }}>Settings</h2>
-				<div
-					style={{
-						display: 'flex',
-						flexDirection: 'column',
-						textDecoration: 'none',
-						gap: '16px',
-						marginBottom: '70px',
-					}}
-				>
-					<Link className="drop-nav-links" to="/account">
-						Account information
-					</Link>
-					<Link className="drop-nav-links" to="/changepassword">
-						Change password
-					</Link>
-					<Link className="drop-nav-links" to="/mypurchases">
-						My purchases
-					</Link>
-					<Link className="drop-nav-links" to="/mylistings">
-						My listings
-					</Link>
-				</div>
-				<br />
-				<h2 style={{ color: '#313131', margin: '0px' }}>Help</h2>
-				<div
-					style={{
-						display: 'flex',
-						flexDirection: 'column',
-						gap: '16px',
-						marginBottom: '70px',
-					}}
-				>
-					<Link className="drop-nav-links" to="/faq">
-						FAQ
-					</Link>
-					<Link className="drop-nav-links" to="/contactus">
-						Contact us
-					</Link>
-				</div>
-				<div className="bottom-drop-links">
-					<Link className="drop-nav-links" to="/">
-						<h3>Sign out</h3>
-					</Link>
-					<button
-						style={{
-							background: 'none',
-							border: 'none',
-						}}
-						onClick={() => isDropOpen(false)}
-					>
-						<img src={logout} alt="logout" />
+		<div>
+			<div className="profile-dropdown">
+				{/* This is what shows initially on header */}
+				<div className="profile">
+					<p style={{ margin: '0', textAlign: 'right', color: '#ffffff' }}>
+						Welcome back,
+					</p>
+					<button className="profile-button" onClick={handleToggle3}>
+						<img src={dropArrow} alt="dropArrow" />
+						<h4 style={{ margin: '0' }}>Pearl The Cat</h4>
 					</button>
 				</div>
+				{isDropOpen3 && (
+					<div ref={dropMenu} onClick={(e) => e.stopPropagation()}>
+						<div className="drop-container">
+							<div className="title-container">
+								<div
+									style={{
+										color: '#ffffff',
+										backgroundColor: '#560f9f',
+										height: '40px',
+										width: '40px',
+										borderRadius: '200px',
+										padding: '10px 15px 10px 15px',
+									}}
+								>
+									P
+								</div>
+								<h1>Pearl The Cat</h1>
+							</div>
+
+							<h2 style={{ color: 'black', margin: '0px' }}>Settings</h2>
+							<div
+								style={{
+									display: 'flex',
+									flexDirection: 'column',
+									textDecoration: 'none',
+									gap: '16px',
+									marginBottom: '70px',
+								}}
+							>
+								<Link className="drop-nav-links" to="/account">
+									Account information
+								</Link>
+								<Link className="drop-nav-links" to="/changepassword">
+									Change password
+								</Link>
+								<Link className="drop-nav-links" to="/mypurchases">
+									My purchases
+								</Link>
+								<Link className="drop-nav-links" to="/mylistings">
+									My listings
+								</Link>
+							</div>
+							<br />
+							<h2 style={{ color: '#313131', margin: '0px' }}>Help</h2>
+							<div
+								style={{
+									display: 'flex',
+									flexDirection: 'column',
+									gap: '16px',
+									marginBottom: '70px',
+								}}
+							>
+								<Link className="drop-nav-links" to="/faq">
+									FAQ
+								</Link>
+								<Link className="drop-nav-links" to="/contactus">
+									Contact us
+								</Link>
+							</div>
+							<div>
+								<Link className="bottom-drop-links" to="/">
+									<h3>Sign out</h3>
+									<img
+										style={{ objectFit: 'contain' }}
+										src={logout}
+										alt="logout"
+									/>
+								</Link>
+							</div>
+						</div>
+					</div>
+				)}
 			</div>
 		</div>
 	);
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,24 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Dropdown from './Dropdown';
 import SearchBar from './Search-bar';
+import Dropdown from './Dropdown';
 
 import altLogo from '../../assets/altLogo.png';
 import bell from '../../assets/Bell.png';
 import plus from '../../assets/Plus.png';
 import hamburger from '../../assets/LinesMenu.png';
-import dropArrow from '../../assets/DownArrow.png';
+
 import './index.css';
 
 export default function Header() {
-	const [isDropOpen, setIsDropOpen] = useState(false);
-	console.log('dropdown: ', isDropOpen);
-
-	const showDrop = () => {
-		setIsDropOpen(true);
-	};
-
 	return (
 		<div className="header">
 			<div className="search-bar-container">
@@ -27,17 +20,9 @@ export default function Header() {
 				</Link>
 
 				<SearchBar />
-
 				<div className="account-links">
-					<div className="profile-dropdown">
-						<p style={{ margin: '0', textAlign: 'right', color: '#ffffff' }}>
-							Welcome back,
-						</p>
-						<button className="profile-button" onClick={showDrop}>
-							<img src={dropArrow} alt="dropArrow" />
-							<h4 style={{ margin: '0' }}>Pearl The Cat</h4>
-						</button>
-					</div>
+					<Dropdown />
+
 					<button style={{ background: 'none', border: 'none' }}>
 						<img alt="notifications" src={bell} />
 					</button>
@@ -66,12 +51,6 @@ export default function Header() {
 					<li>Real Estate</li>
 				</ul>
 			</div>
-			{isDropOpen && (
-				<Dropdown
-					isDropOpen={isDropOpen}
-					onClose={() => setIsDropOpen(false)}
-				/>
-			)}
 		</div>
 	);
 }
