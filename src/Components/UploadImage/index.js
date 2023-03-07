3cc8a1c3956ba5d17ee6093ccf2d92c3e5f6c037
mojonny <moirjonathan@gmail.com>
2023-03-07 15:07:35 -0700

    Minor tweak, will have to come back.

--- src/Components/UploadImage/index.js
+++ src/Components/UploadImage/index.js
@@ -1,6 +1,76 @@
-// import React, { useState } from 'react';
-
 import removeImg from '../../assets/Closing X.svg';
+// import React from 'react';
+import { useState, useEffect } from 'react';
+
+//import loadImg from '../../assets/LoadImg.svg';
+
+//import './index.css';
+
+export default function UploadImage({ src }) {
+	const [file, setFile] = useState([]);
+	const [showDefaultImg, setShowDefaultImg] = useState(true);
+
+	//A way to upload the images
+	const [images, setImages] = useState([]);
+	console.log(images);
+
+	//A way to upload images
+	const [imageURLs, setImageURLs] = useState([]);
+
+	useEffect(() => {
+		if (images.length < 1) return;
+		const newImageUrls = [];
+		images.forEach((image) => newImageUrls.push(URL.createObjectURL(image)));
+		setImageURLs(newImageUrls);
+	}, [images]);
+
+	function onImageChange(e) {
+		setImages([...e.target.files]);
+		setShowDefaultImg(false);
+		console.log(setImages);
+	}
+
+	function deleteFile(e) {
+		const s = file.filter((item, index) => index !== e);
+		setFile(s);
+		console.log(s);
+	}
+	return (
+		<>
+			<label>
+				{showDefaultImg && (
+					<img
+						src={src}
+						alt="main-listing-pic"
+						showDefaultImg={showDefaultImg}
+					/>
+				)}
+				<input type="file" multiple accept="image/*" onChange={onImageChange} />
+
+				{imageURLs.map((imageSrc, index) => {
+					return (
+						<div key={index}>
+							<img src={imageSrc} alt="uploaded img" />
+							<button
+								type="button"
+								style={{
+									background: 'none',
+									border: 'none',
+									position: 'relative',
+								}}
+								onClick={() => deleteFile(index) || setShowDefaultImg(true)}
+							>
+								<img alt="close-button" src={removeImg} />
+							</button>
+						</div>
+					);
+				})}
+			</label>
+		</>
+	);
+}
+
+// import React, { useState } from 'react';
 
 // const UploadImage = ({ src }) => {
 // 	const [file, setFile] = useState([]);
@@ -79,74 +149,3 @@ import removeImg from '../../assets/Closing X.svg';
 // };
 
 // export default UploadImage;
-
-// import React from 'react';
-import { useState, useEffect } from 'react';
-
-//import loadImg from '../../assets/LoadImg.svg';
-
-//import './index.css';
-
-export default function UploadImage({ src }) {
-	const [file, setFile] = useState([]);
-	const [showDefaultImg, setShowDefaultImg] = useState(true);
-
-	//A way to upload the images
-	const [images, setImages] = useState([]);
-	console.log(images);
-
-	//A way to upload images
-	const [imageURLs, setImageURLs] = useState([]);
-
-	useEffect(() => {
-		if (images.length < 1) return;
-		const newImageUrls = [];
-		images.forEach((image) => newImageUrls.push(URL.createObjectURL(image)));
-		setImageURLs(newImageUrls);
-	}, [images]);
-
-	function onImageChange(e) {
-		setImages([...e.target.files]);
-		setShowDefaultImg(false);
-		console.log(setImages);
-	}
-
-	function deleteFile(e) {
-		const s = file.filter((item, index) => index !== e);
-		setFile(s);
-		console.log(s);
-	}
-	return (
-		<>
-			<label>
-				{showDefaultImg && (
-					<img
-						src={src}
-						alt="main-listing-pic"
-						showDefaultImg={showDefaultImg}
-					/>
-				)}
-				<input type="file" multiple accept="image/*" onChange={onImageChange} />
-
-				{imageURLs.map((imageSrc, index) => {
-					return (
-						<div key={index}>
-							<img src={imageSrc} alt="uploaded img" />
-							<button
-								type="button"
-								style={{
-									background: 'none',
-									border: 'none',
-									position: 'relative',
-								}}
-								onClick={() => deleteFile(index) || setShowDefaultImg(true)}
-							>
-								<img alt="close-button" src={removeImg} />
-							</button>
-						</div>
-					);
-				})}
-			</label>
-		</>
-	);
-}
