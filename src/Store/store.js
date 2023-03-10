34ae72e63f236211ef72d73a9e6c9816b2e00650
mojonny <moirjonathan@gmail.com>
2023-03-10 13:23:30 -0700

    css edits.

--- src/Store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './counterSlice'; //import our reducer from step 4
-
-export default configureStore({
-	reducer: {
-		counter: counterReducer, //add our reducer from step 4
-	},
-});
